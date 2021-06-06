7dc46fb07af6c6dd2c5a094e15bc07978d1a7f4d
immcoc1 <immcoc1@naver.com>
2021-06-06 21:48:14 +0900

    Add files via upload

--- include/backtrack.h
+++ include/backtrack.h
@@ -55,41 +55,6 @@ public:
     };
 
 
-    int Get_HashVal(std::vector<std::pair<Vertex, Vertex>> emb) {
-        int h_val = 0;
-        for (int i = 0; i < (int)emb.size(); i++) {
-            h_val += (emb[i].first + 1) * emb[i].second;
-        }
-        return h_val % 200000;
-    }
-
-    bool Insert_Hash(int h_val, std::vector<std::pair<Vertex, Vertex>> emb) {
-        while (true) {
-            if (hashmap.find(h_val) == hashmap.end()) {
-                // hash.insert(hash_map<int, std::vector<std::pair<Vertex, Vertex>>>::value_type(h_val, emb));
-                hashmap.insert(std::pair<int, std::vector<std::pair<Vertex, Vertex>>>(h_val, emb));
-                return true;
-            }
-            else {
-
-                if (Compare_Embedding(hashmap.find(h_val)->second, emb)) {
-                    return false;
-                }
-                else {
-                    h_val = (h_val + 7) % 200000;
-                }
-            }
-        }
-    }
-
-    bool Compare_Embedding(std::vector<std::pair<Vertex, Vertex>> emb1, std::vector<std::pair<Vertex, Vertex>> emb2) {
-        for (int i = 0; i < (int)emb1.size(); i++) {
-            if (emb1[i].second != emb2[i].second) {
-                return false;
-            }
-        }
-        return true;
-    }
 
 
 
